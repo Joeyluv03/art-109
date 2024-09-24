68f18b4468109897bb62b3315bb015f0c1a9b7b0
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-09-24 14:59:02 -0700

    dom

---
+++ dom/js/main.js
@@ -0,0 +1,11 @@
+//make images disappear on click
+
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-1").style.display = "none";
+});
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-2").style.display = "none";
+});
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-3").style.display = "none";
+});
