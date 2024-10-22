aac85e747e6457526f7f4e3102b0d436a695562f
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-10-22 15:59:32 -0700

    new

--- three-00-starter/js/main.js
+++ three-00-starter/js/main.js
@@ -5,23 +5,25 @@
 import * as THREE from 'three';
 
 // Import add-ons
-import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+//import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
 
+//~~~~~~~~~~~~~~~~global variables~~~~~~~~~~~~~~~~
 let scene, camera, renderer, cube;
+let sceneContainer = document.querySelector("scene-container");
 
 // ~~~~~~~~~~~~~~~~Create scene here~~~~~~~~~~~~~~~~
-function init(){
+function init() {
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera(75, sceneContainer.clientWidth / sceneContainer.clientHeight, 0.1, 1000);
 
     //rendering
-    
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setAnimationLoop( animate );
-    document.body.appendChild( renderer.domElement );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
+    renderer.setAnimationLoop(animate);
+    sceneContainer.appendChild(renderer.domElement);
 
     //lights
     const light = new THREE.directionalLight(0xffffff, 3);
@@ -34,27 +36,27 @@ function init(){
     const lightleft = new THREE.directionalLight(0xffffff, 3);
     lightleft.position.set(-3, 4, 5);
     scene.add(lightleft);
-    
+
     const helperleft = new THREE.directionalLightHelper(lightleft, 5);
     scene.add(helperleft);
 
     //initiate add ons
-    const controls = new OrbitControls(camera, renderer.domElement);
+    //const controls = new OrbitControls(camera, renderer.domElement);
     const loader = new GLTFLoader(); // to load 3d models
 
-    loader.load('assets/fish.gltf', function(gltf){
+    loader.load('assets/fish.gltf', function (gltf) {
         const fish = gltf.scene
         scene.add(fish);
     })
-    
+
     geometry
-    const geometry = new THREE.CapsuleGeometry( 1, 1, 1, 8 );
+    const geometry = new THREE.CapsuleGeometry(1, 1, 1, 8);
 
     const texture = new THREE.TextureLoader().load('texture/ice.jpg');
     const material = new THREE.MeshStandardMaterial({ map: texture });
     cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-        
+
     //camera position
     camera.position.z = 5;
 }
@@ -64,19 +66,19 @@ function animate() {
 
     requestAnimationFrame(animate);
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 
 }
 
 
 
-function onWindowResize(){
-    camera.aspect = window.innerWidth / window.innerHeight;
+function onWindowResize() {
+    camera.aspect = sceneContainer.clientWidth, sceneContainer.clientHeight;
     camera.updateProjectMatrix();
-    renderer.setSize(window.innerWidth , window.innerHeight);
+    renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
 
 }
 
