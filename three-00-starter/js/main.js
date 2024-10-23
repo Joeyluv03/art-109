b8871d2109503c0f4547cb4019abeebb2ffc01ed
Joeyluv03 <90421255+Joeyluv03@users.noreply.github.com>
2024-10-22 19:40:55 -0700

    new

--- three-00-starter/js/main.js
+++ three-00-starter/js/main.js
@@ -1,73 +1,121 @@
-//\ Basic Three.JS scene from documentation, importing Three.JS through a CDN 
-// https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
-
 //~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
 import * as THREE from 'three';
 
 // Import add-ons
-//import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
 
 //~~~~~~~~~~~~~~~~global variables~~~~~~~~~~~~~~~~
-let scene, camera, renderer, cube;
+let scene, camera, renderer, ball, fish, mixer;
 let sceneContainer = document.querySelector("scene-container");
 
 // ~~~~~~~~~~~~~~~~Create scene here~~~~~~~~~~~~~~~~
 function init() {
+
+    // ~~~~~~~~~~~~~~~~Set up Scene~~~~~~~~~~~~~~~~
     scene = new THREE.Scene();
+
+    //change scene bg color
+    scene.background = new THREE.Color(0x1d2b38);
 
     camera = new THREE.PerspectiveCamera(75, sceneContainer.clientWidth / sceneContainer.clientHeight, 0.1, 1000);
 
-    //rendering
+    // ~~~~~~~~~~~~~~~~Rendering~~~~~~~~~~~~~~~~
 
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
-    renderer.setAnimationLoop(animate);
     sceneContainer.appendChild(renderer.domElement);
+    renderer.setAnimationLoop(animate);
 
-    //lights
-    const light = new THREE.directionalLight(0xffffff, 3);
-    light.position.set(3, 4, 5);
-    scene.add(light);
+    // ~~~~~~~~~~~~~~~~Add Lights(Right)~~~~~~~~~~~~~~~~
+    const lightRight = new THREE.directionalLight(0xffffff, 4);
+    lightRight.position.set(3, 4, 5);
+    scene.add(lightRight);
 
-    const helper = new THREE.directionalLightHelper(light, 5);
-    scene.add(helper);
+    // ~~~~~~~~~~~~~~~~Light Helper~~~~~~~~~~~~~~~~
+    // const helper = new THREE.directionalLightHelper(light, 5);
+    // scene.add(helper);
 
-    const lightleft = new THREE.directionalLight(0xffffff, 3);
-    lightleft.position.set(-3, 4, 5);
-    scene.add(lightleft);
+    // const lightleft = new THREE.directionalLight(0xffffff, 3);
+    // lightleft.position.set(-3, 4, 5);
+    // scene.add(lightleft);
 
-    const helperleft = new THREE.directionalLightHelper(lightleft, 5);
-    scene.add(helperleft);
+    // const helperleft = new THREE.directionalLightHelper(lightleft, 5);
+    // scene.add(helperleft);
 
-    //initiate add ons
-    //const controls = new OrbitControls(camera, renderer.domElement);
+    // ~~~~~~~~~~~~~~~~Add Lights(Left)~~~~~~~~~~~~~~~~
+    const lightLeft = new THREE.directionalLight(0xffffff, 4);
+    lightLeft.position.set(-3, 2, 3);
+    scene.add(lightLeft);
+
+
+    // ~~~~~~~~~~~~~~~~Initiate add-ons~~~~~~~~~~~~~~~~
+    const controls = new OrbitControls(camera, renderer.domElement);
     const loader = new GLTFLoader(); // to load 3d models
 
-    loader.load('assets/fish.gltf', function (gltf) {
-        const fish = gltf.scene
-        scene.add(fish);
-    })
 
-    geometry
-    const geometry = new THREE.CapsuleGeometry(1, 1, 1, 8);
+    // ~~~~~~~~~~~~~~~~Geometry~~~~~~~~~~~~~~~~
+    const geometry = new THREE.SphereGeometry(.2, 32, 16);
 
     const texture = new THREE.TextureLoader().load('texture/ice.jpg');
     const material = new THREE.MeshStandardMaterial({ map: texture });
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+
+    ball = new THREE.Mesh(geometry, material);
+    scene.add(ball);
+
+    //load 3d model
+    loader.load('assets/fish.gltf', function (gltf) {
+        fish = gltf.scene;
+        scene.add(fish);
+        fish.scale.set(2, 2, 2);
+        fish.position.y = -1.5;
+        
+        //Animation  
+        mixer = new THREE.AnimationMixer(fish);
+        const clips = gltf.animations;
+        
+        //load + play animation
+        const clipSwim = new THREE.AnimationClip.findByName(clips, 'fishAction');
+        const action = mixer.clipAction(clipSwim);
+        action.play();
+
+        clips.forEach(function (clipSwim) {
+            const action = mixer.clipAction(clipSwim);
+            action.play();
+        });
+
+    })
 
     //camera position
     camera.position.z = 5;
 }
 
 
+const clock = new THREE.Clock();
+
 function animate() {
 
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    ball.rotation.x += 0.007;
+    ball.rotation.y += 0.007;
+
+    ball.position.x = Math.sin(Date.now() / 5000) * 2;
+    ball.position.y = Math.sin(Date.now() / 3000) * 2;
+    ball.position.z = Math.sin(Date.now() / 4000) * 2;
+
+    if (fish) {
+        //fish.rotation.x += 0.007;
+        // fish.rotation.y += 0.007;
+        
+        // animation mixer update
+        mixer.update(clock.getDelta());
+
+        fish.rotation.y = Math.sin(Date.now() / 500) * 2;
+    }
+
+    if (mixer)
+        mixer.update(clock.getDelta());
 
     renderer.render(scene, camera);
 
@@ -76,7 +124,7 @@ function animate() {
 
 
 function onWindowResize() {
-    camera.aspect = sceneContainer.clientWidth, sceneContainer.clientHeight;
+    camera.aspect = sceneContainer.clientWidth / sceneContainer.clientHeight;
     camera.updateProjectMatrix();
     renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
 
@@ -86,13 +134,5 @@ window.addEventListener('resize', onWindowResize, false);
 
 init();
 animate();
-
-
-// ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
-// const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader(); // to load 3d models
-
-
-// →→→→→→ Follow next steps in tutorial: // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 
 
